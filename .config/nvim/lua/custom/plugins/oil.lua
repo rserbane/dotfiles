d51cf1c83579c7c478b240e7d47763643fd1f7ce
rserbane <razvan@serbane.dev>
2025-06-20 22:58:30 -0400

    remove kickstart health and add oil keybindings

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -7,5 +7,22 @@ return {
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
 	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	config = function()
+		require("oil").setup({
+			columns = { "icon" },
+			keymaps = {
+				["<C-h>"] = false,
+			},
+			view_options = {
+				show_hidden = true,
+			},
+		})
+
+		-- Open parent directory in current window
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+		-- Open parent directory in floating window
+		vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+	end,
 	lazy = false,
 }
