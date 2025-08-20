cda5ca2c879497946129cffcbead315cad7e7d1b
rserbane <razvan@serbane.dev>
2025-08-19 23:36:45 -0400

    update neogit keymaps

--- .config/nvim/lua/custom/plugins/neogit.lua
+++ .config/nvim/lua/custom/plugins/neogit.lua
@@ -5,4 +5,9 @@ return {
 		"sindrets/diffview.nvim", -- optional - Diff integration
 		"nvim-telescope/telescope.nvim", -- optional
 	},
+	config = function()
+		vim.keymap.set("n", "<leader>gs", "<cmd>Neogit<cr>", { desc = "Git Status (Neogit)" })
+		vim.keymap.set("n", "<leader>gd", "<cmd>DiffviewOpen<cr>", { desc = "Git Diffview Open" })
+		vim.keymap.set("n", "<leader>gD", "<cmd>DiffviewClose<cr>", { desc = "Git Diffview Close" })
+	end,
 }
